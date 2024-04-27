7b4cd8883748254b57a899c6a84ad5d22c725a74
Krešimir Nežmah <keko3700@gmail.com>
2024-04-27 21:57:10 +0200

    maknuo n iz inputa

--- koreografija/nezmah.cpp
+++ koreografija/nezmah.cpp
@@ -55,7 +55,7 @@ void solve (vi &v) {
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cin >> n;
+    n = 1000;
     vi v(n);
     for (int i = 0; i < n; i++) v[i] = i + 1;
     solve(v);
