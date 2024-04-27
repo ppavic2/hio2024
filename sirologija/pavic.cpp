8d6a1210618f84b8dade2d5578f8c00265fcf34a
Patrick Pavic <patrik.pavic2@gmail.com>
2024-04-27 18:27:34 +0200

    sirologija sluzbeno

---
+++ sirologija/pavic.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+const int N = 5050;
+
+int n, m, sol, a[N][N];
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	cin >> n >> m;
+	for(int i = 0;i < n;i++) {
+		cin >> s;
+		for(int j = 0;j < m;j++)
+			a[i][j] = s[j] == '#';
+	}
+	for(int i = 0;i < n;i++)
+		for(int j = (!i);j < m;j++)\
+			a[i][j] |= (i == 0 || a[i - 1][j]) && (j == 0 || a[i][j - 1]);
+	for(int i = n - 1;i >= 0;i--)
+		for(int j = m - 1 - (i == n - 1);j >= 0;j--)
+			a[i][j] |= (i + 1 == n || a[i + 1][j]) && (j + 1 == m || a[i][j + 1]);
+	if(a[0][0]) { printf("0\n"); return 0; }
+	for(int i = 1;i < n;i++)
+		for(int j = 1;j < m;j++)
+			if(a[i][j]) sol += (!a[i - 1][j]) && (!a[i][j - 1]) && (!a[i - 1][j - 1]);
+	printf("%d\n", sol + 1);
+	return 0;
+}
