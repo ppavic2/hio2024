c99061ecf32bd65b5f4baafd61b128ba9e1c8e9a
Patrick Pavic <patrik.pavic2@gmail.com>
2024-04-27 22:24:55 +0200

    sirologija/

--- sirologija/gen.cpp
+++ sirologija/gen.cpp
@@ -2,11 +2,10 @@
 #include <chrono>
 #include <iostream>
 #include <random>
+#include <cassert>
 #include <cstring>
 
 using namespace std;
-
-int n, m;
 
 mt19937 rng;
 
@@ -33,39 +32,110 @@ SUBTASKOVI:
 
 int to_int(char *s){
     int ret = 0, k = strlen(s);
-    for(int i = 0;i < k;i++)
+    for(int i = 0;i < k;i++) {
         ret = 10 * ret + s[i] - '0';
+    }
    	return ret;
 }
 
 const int N = 2050;
 
 char ans[N][N];
+int n, m;
+
+void random_path(int x, int y) {
+	ans[x][y] = '.';
+	if(x == n - 1 && y == m - 1) return;
+	if(x == n - 1) { random_path(x, y + 1); return; }
+	if(y == m - 1) { random_path(x + 1, y); return; }
+	int z = rint(0, 1);
+	random_path(x + z, y + 1 - z);
+}
 
 int main(int argc, char** argv){
     int tip = to_int(argv[1]);
     int dens = to_int(argv[2]);
+    int gen_type = to_int(argv[3]);
 	rng = mt19937(chrono::steady_clock::now().time_since_epoch().count());
 	
 	int rub = (tip == 3 || tip == 5);
 	
 	int delta[7] = {-1, 100, 5, 10, 10, 100, 100};
 	int maxn[7] = {-1, 2000, 10, 50, 50, 2000, 2000};
+	int prazn[7] = {-1, 0, 0, 6, 6, 50, 50};
 	
-	int n = rint(maxn[tip] - delta[tip], maxn[tip]);
-	int m = rint(maxn[tip] - delta[tip], maxn[tip]);
+	n = rint(maxn[tip] - delta[tip], maxn[tip]);
+	m = rint(maxn[tip] - delta[tip], maxn[tip]);
+	int praz = prazn[tip];
 	
-	int pos_red = rint(1, n - 1);
-	memset(ans, '.', sizeof(ans));
-	for(int i = 0;i < n;i++) {
-		for(int j = 0;j < n;j++) {
-			if(rub && (i == 0 || i == n - 1 || j == 0 || j == m - 1)) continue;
-			if((i == 0 && j == 0) || (i == n - 1 && j == m - 1)) continue;
-			if(tip == 1 && i != pos_red) continue;
-			ans[i][j] = chp(dens);
+
+	
+	if(gen_type == 0) {
+		int pos_red = rint(1, n - 1);
+		memset(ans, '.', sizeof(ans));
+		for(int i = 0;i < n;i++) {
+			for(int j = 0;j < m;j++) {
+				if(rub && (i == 0 || i == n - 1 || j == 0 || j == m - 1)) continue;
+				if((i == 0 && j == 0) || (i == n - 1 && j == m - 1)) continue;
+				if(n > 30 && ((i < praz && j < praz) || (n - i < praz + 1 && n - j < praz + 1))) continue;
+				if(tip == 1 && i != pos_red) continue;
+				ans[i][j] = chp(dens);
+			}
 		}
+	} else if(gen_type == 1) {
+		assert(tip != 1);
+		assert(dens > 0);
+		memset(ans, '#', sizeof(ans));
+		for(int i = 0;i < dens;i++) {
+			random_path(0, 0);
+		}
+		for(int i = 0;i < (n / 3) * n;i++)
+			ans[rint(0, n - 1)][rint(0, m - 1)] = '.';
+	} else if(gen_type == 2) {
+		int kad = rint(0, 1);
+		memset(ans, '.', sizeof(ans));
+		for(int i = 0;i < n;i++) {
+			for(int j = 0;j < m;j++) {
+				if(rub && (i == 0 || i == n - 1 || j == 0 || j == m - 1)) continue;
+				if((i == 0 && j == 0) || (i == n - 1 && j == m - 1)) continue;
+				if((((kad ^ i) | (kad ^ j))) & 1) continue;
+				ans[i][j] = chp(dens);
+			}
+		}
+	} else if(gen_type == 3) {
+		int kad = rint(0, 1);
+		memset(ans, '.', sizeof(ans));
+		for(int i = 0;i < n;i++) {
+			for(int j = 0;j < m;j++) {
+				if(rub && (i == 0 || i == n - 1 || j == 0 || j == m - 1)) continue;
+				if((i == 0 && j == 0) || (i == n - 1 && j == m - 1)) continue;
+				int tdens = dens;
+				if((((kad ^ i) | (kad ^ j))) & 1) tdens = 1;
+				ans[i][j] = chp(tdens);
+			}
+		}
+	} else if(gen_type == 4) {
+		memset(ans, '.', sizeof(ans));	
+	} else if(gen_type == 5) {
+		int pos_red = rint(1, n - 1);
+		if(tip == 1) {
+			memset(ans, '.', sizeof(ans));
+			for(int j = 0;j < m;j++) ans[pos_red][j] = '#';
+		} else {
+			memset(ans, '#', sizeof(ans));
+			random_path(0, 0);
+			for(int i = 0;i < n;i++) {
+				for(int j = 0;m - j - 1 > j;j++)
+					swap(ans[i][j], ans[i][m - j - 1]);
+			}
+			for(int i = 0;i < n;i++)
+				for(int j = 0;j < m;j++)
+					ans[i][j] = (ans[i][j] == '#') ? '.' : '#';
+			ans[0][0] = '.'; ans[n - 1][m - 1] = '.';
+		}
+	} else {
+		assert(false);
 	}
-	
 	printf("%d %d\n", n, m);
 	for(int i = 0;i < n;i++) {
 		for(int j = 0;j < m;j++) {
--- sirologija/napravi_primjere.sh
+++ sirologija/napravi_primjere.sh
@@ -1,6 +1,67 @@
+zadatak="sirologija"
+
+
+((n = 1))
+
+./gen $n 10 0 > ./test/"$zadatak".in."$n"a
+./gen $n 10 0 > ./test/"$zadatak".in."$n"b
+./gen $n 10 0 > ./test/"$zadatak".in."$n"c
+./gen $n 10 0 > ./test/"$zadatak".in."$n"d
+./gen $n 50 0 > ./test/"$zadatak".in."$n"e
+./gen $n 50 0 > ./test/"$zadatak".in."$n"f
+./gen $n 50 0 > ./test/"$zadatak".in."$n"g
+./gen $n 50 0 > ./test/"$zadatak".in."$n"h
+./gen $n 50 0 > ./test/"$zadatak".in."$n"i
+./gen $n 50 0 > ./test/"$zadatak".in."$n"j
+./gen $n 90 0 > ./test/"$zadatak".in."$n"k
+./gen $n 90 0 > ./test/"$zadatak".in."$n"l
+./gen $n 90 0 > ./test/"$zadatak".in."$n"m
+./gen $n 90 0 > ./test/"$zadatak".in."$n"n
+./gen $n 90 0 > ./test/"$zadatak".in."$n"o
+./gen $n 0 4 > ./test/"$zadatak".in."$n"p
+
+broj1=(-1 -1 3 20 20 1000 1000 )
+broj2=(-1 -1 5 50 50 2000 2000 )
+broj3=(-1 -1 10 100 100 3000 3000 )
+
+for ((n=2;n<=6;n++))
+do
+	./gen $n 10 0 > ./test/"$zadatak".in."$n"a
+	./gen $n 10 0 > ./test/"$zadatak".in."$n"b
+	./gen $n 20 0 > ./test/"$zadatak".in."$n"c
+	./gen $n 20 0 > ./test/"$zadatak".in."$n"d
+	./gen $n 25 0 > ./test/"$zadatak".in."$n"e
+	./gen $n 25 0 > ./test/"$zadatak".in."$n"f
+	
+	./gen $n ${broj1[$n]} 1 > ./test/"$zadatak".in."$n"g
+	./gen $n ${broj1[$n]} 1 > ./test/"$zadatak".in."$n"h
+	./gen $n ${broj2[$n]} 1 > ./test/"$zadatak".in."$n"i
+	./gen $n ${broj2[$n]} 1 > ./test/"$zadatak".in."$n"j
+	./gen $n ${broj3[$n]} 1 > ./test/"$zadatak".in."$n"k
+	./gen $n ${broj3[$n]} 1 > ./test/"$zadatak".in."$n"l
+
+	./gen $n 20 2 > ./test/"$zadatak".in."$n"m
+	./gen $n 30 2 > ./test/"$zadatak".in."$n"n
+	./gen $n 50 2 > ./test/"$zadatak".in."$n"o
+	./gen $n 70 2 > ./test/"$zadatak".in."$n"p
+	./gen $n 99 2 > ./test/"$zadatak".in."$n"q
+	./gen $n 100 2 > ./test/"$zadatak".in."$n"r
+
+	./gen $n 20 3 > ./test/"$zadatak".in."$n"s
+	./gen $n 30 3 > ./test/"$zadatak".in."$n"t
+	./gen $n 50 3 > ./test/"$zadatak".in."$n"u
+	./gen $n 70 3 > ./test/"$zadatak".in."$n"v
+	./gen $n 99 3 > ./test/"$zadatak".in."$n"w
+	./gen $n 100 3 > ./test/"$zadatak".in."$n"x
+	
+	./gen $n 0 4 > ./test/"$zadatak".in."$n"y
+	./gen $n 0 5 > ./test/"$zadatak".in."$n"z
+done
+
 for f in test/*.in*;
 do
   echo $f
   output=$(echo $f | sed 's/in/out/g') 
   ./pavic < $f > $output
+  cat $output
 done
